bd92913dbe77d5d7784e4622a52aa5bc28a6bdb7
Yi Le and Sarah <yileandsarah@gmail.com>
2019-01-04 20:43:24 +0800

    Updated scripts.js to own web app

--- js/scripts.js
+++ js/scripts.js
@@ -218,7 +218,8 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbzUqz44wOat0DiGjRV1gUnRf4HRqlRARWggjvHKWvqniP7eVDG-/exec', data)
+            /***Changed RSVP script URL to Google***/
+            $.post('https://script.google.com/macros/s/AKfycbyhteXsWq3wSCuGSfJjWrMfPMasTBzsM-ENQxhqUUk9XLuCJqk/exec', data)
                 .done(function (data) {
                     console.log(data);
                     $('#alert-wrapper').html('');
